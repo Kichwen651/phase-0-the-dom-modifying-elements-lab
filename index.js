68b0724ffe997544d9f669b0ef755a19325473ff
Victor kichwen <victor.kichwen@student.moringaschool.com>
2024-10-03 21:40:52 +0300

    complete lab 12

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1');
+newHeader.textContent = 'THE CONFIDATION';
+// document.getElementById('h1').append('victory')
+document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = "Victor is the champion"
+
+ 
+
+
